4000f0fccfaff79f0978073c84b008abc9c45577
Cody Epstein <codyepstein@gmail.com>
2024-07-15 17:48:52 +0000

    Added jsx layout to entries

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,20 @@
+import { Route, Routes } from 'react-router'
 import './css/layout.css'
 import './css/reset.css'
 import './css/styles.css'
 
+import { Header } from './components/Header'
+import { EntryList } from './pages/EntryList'
+import { EntryForm } from './pages/EntryForm'
+
 function App() {
   return (
-    <div className='container'>Some text</div>
+    <Routes>
+      <Route path='/' element={<Header />}>
+        <Route index element={<EntryList />}/>
+        <Route path='/entry-form/:entryId' element={<EntryForm />}/>
+      </Route>
+    </Routes>
   )
 }
 
